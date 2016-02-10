fba7bc51b630ed086ce68842d4fc3973f2d9f0e9
Denly <007623@gmail.com>
2016-02-09 22:39:26 -0500

    Initial Commit

---
+++ opencv_hands/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_hands
+//
+//  Created by Juby on 2/9/16.
+//  Copyright (c) 2016 Dan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
